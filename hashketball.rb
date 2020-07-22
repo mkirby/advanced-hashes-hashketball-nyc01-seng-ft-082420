e34e2d005ca1fde1459c718f8420b375b9c904fa
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-22 19:18:11 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -246,10 +246,8 @@ def player_with_longest_name
 end
 
 def long_name_steals_a_ton?
-  player_with_longest_name == player_with_biggest_stat(:steals) ? true : false
+  player_with_longest_name == get_player_with_biggest_stat(:steals) ? true : false
 end
-
-binding.pry
 
 
   
